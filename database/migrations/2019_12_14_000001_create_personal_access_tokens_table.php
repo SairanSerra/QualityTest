e400a4362e58875825b19cdaed9123db6bc76617
Sairan <sairan.serra@gmail.com>
2022-10-15 05:00:15 -0300

    ajuste deploy

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,7 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->string('tokenable');
             $table->string('name');
             $table->string('token', 64);
             $table->text('abilities')->nullable();
